313060d7e7a34e76a31a92f5c86cf5abd18bfe56
Sidinei Silva <sidinei.silva02@gmail.com>
2020-12-21 15:07:08 -0300

    Ajustes eslint das paginas default

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import React from 'react';
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;

79a57ae06de79a272cd18f9eb501c4d58afdbe1b
Sidinei Silva <sidinei.silva02@gmail.com>
2020-12-29 14:08:23 -0300

    Incluindo validação de getUserMedia

--- services/navegatorMedia.ts
+++ services/navegatorMedia.ts
@@ -1,13 +1,15 @@
 export const getMyMediaWebCam = callback => {
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: true })
-    .then(stream => {
-      return callback(null, stream);
-    })
-    .catch(() => {
-      alert('Não foi possivel capturar camera');
-      if (typeof window !== 'undefined') {
-        window.location.replace('/');
-      }
-    });
+  if (typeof window !== 'undefined') {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then(stream => {
+        return callback(null, stream);
+      })
+      .catch(() => {
+        alert('Não foi possivel capturar camera');
+        if (typeof window !== 'undefined') {
+          window.location.replace('/');
+        }
+      });
+  }
 };

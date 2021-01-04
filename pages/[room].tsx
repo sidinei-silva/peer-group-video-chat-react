4f32cc9d742f8225df551a3fe509ef51c41222b4
Sidinei Silva <sidinei.silva02@gmail.com>
2021-01-04 09:24:59 -0300

    Migrando chat para chackra ui

--- pages/[room].tsx
+++ pages/[room].tsx
@@ -1,18 +1,33 @@
+/* eslint-disable react/no-array-index-key */
 import {
+  Box,
   Button,
+  Center,
+  Divider,
+  Flex,
   FormControl,
   FormLabel,
+  Grid,
+  GridItem,
+  Heading,
   Input,
+  ListItem,
   Modal,
   ModalBody,
   ModalContent,
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  UnorderedList,
   useDisclosure,
+  Text,
+  HStack,
+  outline,
+  VStack,
+  Container,
 } from '@chakra-ui/react';
 import { useRouter } from 'next/dist/client/router';
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, useCallback } from 'react';
 import { IoHandRightOutline, IoHandRightSharp } from 'react-icons/io5';
 import { getMyMediaWebCam } from '../services/navegatorMedia';
 import {
@@ -44,6 +59,12 @@ xmlns="http://www.w3.org/2000/svg"
 <path d="M82.42 209.08c15.06-6.62 32.38 1.31 38.5 17.62L156 312h11.27V80c0-17.6 13.3-32 29.55-32 16.26 0 29.55 14.4 29.55 32v151.75l14.78.25V32c0-17.6 13.3-32 29.55-32 16.3 0 29.55 14.4 29.55 32v199.75L315 232V64c0-17.6 13.3-32 29.55-32 16.26 0 29.55 14.4 29.55 32v183.75l14.78.25V128c0-17.6 13.3-32 29.55-32C434.7 96 448 110.4 448 128v216c0 75.8-37.13 168-169 168-40.8 0-79.42-7-100.66-21a121.41 121.41 0 01-33.72-33.31 138 138 0 01-16-31.78L66.16 250.77c-6.11-16.31 1.2-35.06 16.26-41.69z" />
 </svg>`;
 
+interface IChatMessage {
+  position: 'right' | 'left';
+  authorName: string;
+  message: string;
+}
+
 const RoomPage: React.FC = () => {
   const myVideoEl = useRef(null);
   const gridVideoEl = useRef(null);
@@ -52,12 +73,13 @@ const RoomPage: React.FC = () => {
   const [name, setName] = useState('');
   const [modal, setModal] = useState(true);
   const [myHandUp, setMyHandup] = useState(false);
+  const [chatMessages, setChatMessages] = useState<IChatMessage[]>([]);
 
   const videoClasses = 'object-cover h-full w-full relative';
   const router = useRouter();
   const { room } = router.query;
 
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { onClose } = useDisclosure();
 
   const enterRoom = () => {
     if (room && modal && name.length > 0) {
@@ -167,23 +189,8 @@ const RoomPage: React.FC = () => {
       }
     });
 
-    subcribeCreateMessage((err, autorName, message) => {
-      const chatList = document.getElementById('chat-list');
-      const messageItem = document.createElement('li');
-
-      const autorMessage = document.createElement('p');
-      autorMessage.className += 'text-xs font-bold';
-      const autorText = document.createTextNode(autorName);
-      autorMessage.append(autorText);
-      messageItem.append(autorMessage);
-
-      messageItem.className += 'p-1 px-3 text-sm';
-      const itemText = document.createTextNode(message);
-      messageItem.append(itemText);
-
-      chatList.append(messageItem);
-      const windowsChat = document.getElementById('windows-chat');
-      windowsChat.scrollTop = windowsChat.scrollHeight;
+    subcribeCreateMessage((err, authorName, message) => {
+      addMessage({ authorName, message, position: 'left' });
     });
 
     subcribeToggleHandUp((err, userId, isHandUp) => {
@@ -195,6 +202,10 @@ const RoomPage: React.FC = () => {
       }
     });
   }, [modal]);
+
+  useEffect(() => {
+    scrollChatToBottom();
+  }, [chatMessages]);
 
   useEffect(() => {
     const gridVideo = gridVideoEl.current;
@@ -214,6 +225,13 @@ const RoomPage: React.FC = () => {
         break;
     }
   });
+
+  const addMessage = useCallback(
+    ({ authorName, message, position }: IChatMessage) => {
+      setChatMessages(state => [...state, { authorName, message, position }]);
+    },
+    [],
+  );
 
   const keyDownEnter = event => {
     if (event.which === 13) {
@@ -239,18 +257,19 @@ const RoomPage: React.FC = () => {
 
   const handleSendMessage = () => {
     if (inputMessage.length > 0) {
-      const chatList = document.getElementById('chat-list');
-      const messageItem = document.createElement('li');
-      messageItem.className += 'p-1 text-sm text-right';
-      const itemText = document.createTextNode(inputMessage);
-      messageItem.append(itemText);
-      chatList.append(messageItem);
-      const windowsChat = document.getElementById('windows-chat');
-      windowsChat.scrollTop = windowsChat.scrollHeight;
+      setChatMessages([
+        ...chatMessages,
+        { authorName: '', message: inputMessage, position: 'right' },
+      ]);
 
       socketSendMessage(name, inputMessage);
       setInputMessage('');
     }
+  };
+
+  const scrollChatToBottom = () => {
+    const windowsChat = document.getElementById('windows-chat');
+    windowsChat.scrollTop = windowsChat.scrollHeight;
   };
 
   const handleMyHand = () => {
@@ -260,83 +279,119 @@ const RoomPage: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-row h-screen max-h-screen w-screen max-w-screen">
-      <div className="w-full h-full flex flex-col">
-        <div
-          ref={gridVideoEl}
-          className={`bg-black h-full grid grid-flow-row grid-cols-${gridCol} gap-4`}
-        >
-          <div className="relative">
-            <video className={videoClasses} ref={myVideoEl}>
-              <track kind="captions" srcLang="pt-BR" />
-            </video>
-            {myHandUp && (
-              <div className="rounded-lg opacity-1 bg-white p-1 w-12 absolute bottom-0 right-0">
-                <svg
-                  stroke="currentColor"
-                  fill="currentColor"
-                  strokeWidth="0"
-                  viewBox="0 0 512 512"
-                  className="text-blue-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M82.42 209.08c15.06-6.62 32.38 1.31 38.5 17.62L156 312h11.27V80c0-17.6 13.3-32 29.55-32 16.26 0 29.55 14.4 29.55 32v151.75l14.78.25V32c0-17.6 13.3-32 29.55-32 16.3 0 29.55 14.4 29.55 32v199.75L315 232V64c0-17.6 13.3-32 29.55-32 16.26 0 29.55 14.4 29.55 32v183.75l14.78.25V128c0-17.6 13.3-32 29.55-32C434.7 96 448 110.4 448 128v216c0 75.8-37.13 168-169 168-40.8 0-79.42-7-100.66-21a121.41 121.41 0 01-33.72-33.31 138 138 0 01-16-31.78L66.16 250.77c-6.11-16.31 1.2-35.06 16.26-41.69z" />
-                </svg>
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="w-full h-16 p-2 bg-white flex flex-row justify-center">
-          <button
-            onClick={handleMyHand}
-            type="button"
-            className="h-full hover:border-blue-500"
-          >
-            {myHandUp ? (
-              <IoHandRightSharp size="2rem" className="text-blue-500" />
-            ) : (
-              <IoHandRightOutline size="2rem" className="text-blue-500" />
-            )}
-          </button>
-        </div>
-      </div>
-
-      <div className="bg-white w-100">
-        <div className="border-b-2 h-16 text-center flex justify-center p-2">
-          <h4 className=" font-bold text-xl">Chat</h4>
-        </div>
-        <div id="windows-chat" className="h-5/6 flex-grow overflow-y-auto">
-          <ul id="chat-list" className="list-none" />
-        </div>
-        <div className="border-t-2 border-gray-200 px-1 pt-4">
-          <div className="relative">
-            <input
-              onKeyDown={keyDownEnter}
-              value={inputMessage}
-              onChange={event => setInputMessage(event.target.value)}
-              type="text"
-              placeholder="Escreva uma mensagem para todos"
-              className="text-left w-full text-sm focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 px-4 shadow rounded-full py-3"
-            />
-            <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
-              <button
-                onClick={handleSendMessage}
-                type="button"
-                className="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
+    <>
+      <Container maxWidth="100%" height="100vh" p={2}>
+        <Flex height="100%">
+          <Box flex={1}>
+            <Flex direction="column">
+              <Grid
+                ref={gridVideoEl}
+                flex={1}
+                bgColor="black"
+                gap={4}
+                templateColumns={`repeat(${gridCol}, 1fr)`}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="h-6 w-6 transform rotate-90"
+                <div className="relative">
+                  <video className={videoClasses} ref={myVideoEl}>
+                    <track kind="captions" srcLang="pt-BR" />
+                  </video>
+                  {myHandUp && (
+                    <div className="rounded-lg opacity-1 bg-white p-1 w-12 absolute bottom-0 right-0">
+                      <svg
+                        stroke="currentColor"
+                        fill="currentColor"
+                        strokeWidth="0"
+                        viewBox="0 0 512 512"
+                        className="text-blue-500"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M82.42 209.08c15.06-6.62 32.38 1.31 38.5 17.62L156 312h11.27V80c0-17.6 13.3-32 29.55-32 16.26 0 29.55 14.4 29.55 32v151.75l14.78.25V32c0-17.6 13.3-32 29.55-32 16.3 0 29.55 14.4 29.55 32v199.75L315 232V64c0-17.6 13.3-32 29.55-32 16.26 0 29.55 14.4 29.55 32v183.75l14.78.25V128c0-17.6 13.3-32 29.55-32C434.7 96 448 110.4 448 128v216c0 75.8-37.13 168-169 168-40.8 0-79.42-7-100.66-21a121.41 121.41 0 01-33.72-33.31 138 138 0 01-16-31.78L66.16 250.77c-6.11-16.31 1.2-35.06 16.26-41.69z" />
+                      </svg>
+                    </div>
+                  )}
+                </div>
+              </Grid>
+              <div className="w-full h-16 p-2 bg-white flex flex-row justify-center">
+                <button
+                  onClick={handleMyHand}
+                  type="button"
+                  className="h-full hover:border-blue-500"
                 >
-                  <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+                  {myHandUp ? (
+                    <IoHandRightSharp size="2rem" className="text-blue-500" />
+                  ) : (
+                    <IoHandRightOutline size="2rem" className="text-blue-500" />
+                  )}
+                </button>
+              </div>
+            </Flex>
+          </Box>
+          <Box width="20rem">
+            <Flex direction="column" height="100%">
+              <Center paddingY={2}>
+                <Heading size="md" textAlign="center">
+                  Chat
+                </Heading>
+              </Center>
+              <Divider marginX={2} />
+              <Box id="windows-chat" flex={1} overflowY="auto">
+                <UnorderedList
+                  marginLeft={0}
+                  id="chat-list"
+                  listStyleType="none"
+                >
+                  {chatMessages.map((chat, index) => (
+                    <ListItem
+                      key={index}
+                      paddingX={3}
+                      marginY={2}
+                      textAlign={chat.position}
+                    >
+                      <Text as="p" fontSize="xs" fontWeight="bold">
+                        {chat.authorName}
+                      </Text>
+                      <Text as="p">{chat.message}</Text>
+                    </ListItem>
+                  ))}
+                </UnorderedList>
+              </Box>
+              <HStack spacing={0} alignContent="stretch">
+                <Input
+                  onKeyDown={keyDownEnter}
+                  value={inputMessage}
+                  textAlign="left"
+                  fontSize="sm"
+                  onChange={event => setInputMessage(event.target.value)}
+                  placeholder="Escreva uma mensagem para todos"
+                  boxShadow="md"
+                  borderLeftRadius={0}
+                  borderRightRadius={0}
+                  size="lg"
+                  _focus={{ boxShadow: 'sm' }}
+                />
+                <Button
+                  size="lg"
+                  boxShadow="md"
+                  borderRightRadius={0}
+                  borderLeftRadius={0}
+                  colorScheme="blue"
+                  onClick={handleSendMessage}
+                  type="button"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="h-6 w-6 transform rotate-90"
+                  >
+                    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+                  </svg>
+                </Button>
+              </HStack>
+            </Flex>
+          </Box>
+        </Flex>
+      </Container>
       <Modal
         isCentered
         closeOnOverlayClick={false}
@@ -364,7 +419,7 @@ const RoomPage: React.FC = () => {
           </ModalFooter>
         </ModalContent>
       </Modal>
-    </div>
+    </>
   );
 };
 

b04daa518bd75fbe48bab0b12ec2b3d73b493c44
Sidinei Silva <sidinei.silva02@gmail.com>
2021-01-05 16:50:32 -0300

    Removendo servidor turn

--- services/webpeers.ts
+++ services/webpeers.ts
@@ -11,28 +11,27 @@ const createMyPeer = () => {
     const { Peer: PeerGlobal }: any = window;
     Peer = PeerGlobal;
     const securePeer = peerPort === '443';
-    // myPeer = new Peer(undefined, {
-    //   path: '/peerjs',
-    //   host: peerHost,
-    //   port: peerPort,
-    //   secure: securePeer,
-    //   debug: 3,
-    //   config: {
-    //     iceServers: [
-    //       { url: 'stun:108.177.98.127:19302' },
-    //       { url: 'stun:stun.l.google.com:19302' },
-    //       {
-    //         url: 'turn:numb.viagenie.ca',
-    //         credential: 'muazkh',
-    //         username: 'webrtc@live.com',
-    //       },
-    //     ],
-    //   },
-    // });
-    myPeer = new Peer(null, {
+    myPeer = new Peer(undefined, {
+      path: '/peerjs',
+      host: peerHost,
+      port: peerPort,
+      secure: securePeer,
       debug: 3,
+      config: {
+        iceServers: [
+          { url: 'stun:stun.l.google.com:19302' },
+          {
+            url: 'turn:numb.viagenie.ca',
+            credential: 'muazkh',
+            username: 'webrtc@live.com',
+          },
+        ],
+      },
     });
-  }
+  //   myPeer = new Peer(null, {
+  //     debug: 3,
+  //   });
+  // }
 };
 
 export const showPeer = () => {

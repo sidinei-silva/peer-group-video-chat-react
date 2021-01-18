6c205438faec3cc8b6b95a6925e7dd7cf90f0007
Sidinei Silva <sidinei.silva02@gmail.com>
2021-01-18 11:58:32 -0300

    Criando envio de evento de usuario transmitindo

--- services/websocket.ts
+++ services/websocket.ts
@@ -73,3 +73,7 @@ export const subcribeUsersInRoom = callback => {
 export const sendGetUsers = () => {
   socket.emit('get-users', {});
 };
+
+export const userStartTransmitting = () => {
+  socket.emit('user-start-transmitting', {});
+};
